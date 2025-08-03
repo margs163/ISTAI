e2de9c10ade946929be8763d59df8c1765cae829
margs163 <danial.aldanov@gmail.com>
2025-08-03 16:54:04 +0500

    damn

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Montserrat, Roboto, Inter, Geist, Geist_Mono } from "next/font/google";
+import { Montserrat, Roboto, Inter, Geist } from "next/font/google";
 import "./globals.css";
 
 const montFont = Montserrat({
