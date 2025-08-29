396e4c57ede61a0ca1861db1da1d486f2919dd73
margs163 <danial.aldanov@gmail.com>
2025-08-29 18:44:53 +0500

    fucking CORS

--- client/middleware.ts
+++ client/middleware.ts
@@ -15,7 +15,20 @@ export default async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
-  return NextResponse.next();
+  const res = NextResponse.next();
+  res.headers.append("Access-Control-Allow-Credentials", "true");
+  res.headers.append(
+    "Access-Control-Allow-Origin",
+    "https://10e186148c02.ngrok-free.app"
+  ); // replace this your actual origin
+  res.headers.append(
+    "Access-Control-Allow-Methods",
+    "GET,DELETE,PATCH,POST,PUT,OPTIONS"
+  );
+  res.headers.append(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Set-Cookie"
+  );
 }
 
 export const config = {
