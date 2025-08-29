8b4387cd38b63413ec970496413467ee0b4c6c4e
margs163 <danial.aldanov@gmail.com>
2025-08-29 19:10:19 +0500

    cors fuck you

--- client/middleware.ts
+++ client/middleware.ts
@@ -15,22 +15,7 @@ export default async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
-  const response = NextResponse.next();
-  response.headers.set(
-    "Access-Control-Allow-Origin",
-    "https://10e186148c02.ngrok-free.app"
-  );
-  response.headers.set("Access-Control-Allow-Credentials", "true");
-  response.headers.set(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  response.headers.set(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Set-Cookie"
-  );
-
-  return response;
+  return NextResponse.next();
 }
 
 export const config = {
--- client/next.config.ts
+++ client/next.config.ts
@@ -9,30 +9,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/(.*)",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://10e186148c02.ngrok-free.app",
-          }, // replace this your actual origin
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT,OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
